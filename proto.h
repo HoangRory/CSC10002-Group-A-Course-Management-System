0e612a253c82cf44b407e3e715cd51eba4a0f162
MinhTran <20melios22@gmail.com>
2023-03-21 17:09:44 +0700

    Something bug before pull new data from main

--- proto.h
+++ proto.h
@@ -1,95 +1,82 @@
-#include <fstream>
-#include <string>
 #include <iostream>
+#include <string>
+#include <fstream>
 
 using namespace std;
 
-struct Account {
+struct Account
+{
     string username = "", password = "";
     string firstName = "", lastName = "";
-    char Gender; //M is male, F is female
-    int SociaID[12];
-    int role; // role = 1 Student, role = 2 staff or teacher
-    Account *next, *prev;
-    Account () {
-        next = prev = nullptr;
-    } 
+    string Gender; // M is male, F is female
+    string SocialID;
+    int role; // 1 student, 2 teacher, 3 staff.
+    Account *next = nullptr, *prev = nullptr;
 };
 
-struct ScoreBoard { 
-    double totalMark, finalMark, midMark, otherMark;
+struct ScoreBoardCourse
+{
+    double totalMark = 0, finalMark = 0, midMark = 0, otherMark = 0;
 };
 
-//student của một course
-struct StudentCourse {
-    int ID[8];
-    string ClassName = "";
-    StudentCourse *next;
-    StudentCourse *prev;
-    StudentCourse() {
-        next = prev = nullptr;
-    }
-    ScoreBoard ScoreBoard;
+// student của một course
+struct StudentCourse
+{
+    string ID = "";
+    string FullName = "";
+    StudentCourse *next = nullptr, *prev = nullptr;
+    ScoreBoardCourse ScoreBoardCourse;
 };
 
-struct Course {
-    int noSemester; // eg this course is in semester 1 thì noSemester = 1
-    int CourseID; //
-    int Credits = 0, maxStudents = 50, numStudents = 0;
+struct Course
+{
+    string Name = "";
+    string CourseID = ""; // eg CSC10002
+    int Credits = 0, maxStudents = 70, numStudents = 0;
     string Room = "";
-    string Name = ""; // eg CSC10002
     string TeacherName = "";
-    string Day = ""; //eg MON TUE...
-    int Session; // eg S1 then session = 1
+    string Day = ""; // eg MON TUE...
+    int Session;     // eg S1 then session = 1
     StudentCourse *StudentCourse;
-    Course *next, *prev;
-    Course() {
-        next = prev = nullptr;
-    }
+    Course *next = nullptr, *prev = nullptr;
 };
-
-//student của toàn trường, được lưu trong từng lớp
-struct Student {
-    int ID[8];
+// student của toàn trường, được lưu trong từng lớp
+struct Student
+{
+    Account *accStudent = nullptr;
+    string ID;
     string ClassName = "";
-    Course *Course1st;
-    Student *next;
+    Course *Course1st = nullptr;
+    Student *next = nullptr, *prev = nullptr;
     Student *prev;
-    Student() {
-        next = prev = nullptr;
-    }
-    ScoreBoard ScoreBoard;
 };
 
-struct Class {
+struct Class
+{
     string Name; // eg 22CLC02
     Student *StudentClass;
-    Class *next, *prev;
-    Class() {
-        next = prev = nullptr;
-    }
+    Class *next = nullptr, *prev = nullptr;
 };
 
-
-
-struct Semester {
+// hết -1 một môn, -2 kì, -3 năm
+struct Semester
+{
     int No; // eg semester 1 thì No = 1
-    int yearStartSchool; 
+    // int Year; // only the start year, when cout put the end year in by adding 1
     string startDate, endDate;
-    Semester *next, *prev;
-    Semester() {
-        next = prev = nullptr;
-    }
+    Course *Course = nullptr;
+    Semester *next = nullptr, *prev = nullptr;
 };
 
-struct Year {
-    int yearStart, yearEnd;
-    Class *Class;
-    Semester *NoSemester;    
+struct Year
+{
+    int yearStart;
+    Class *Class = nullptr;
+    Semester *NoSemester = nullptr;
+    Year *next = nullptr;
 };
 
-
-/* 
+/*
 1 file lưa toàn bộ sinh sinh viên trường
 1 file lưu tên các số kì, số lớp và số khóa, tên kì, tên lớp
 */
