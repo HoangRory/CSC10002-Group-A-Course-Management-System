2f231e09d9398c187d3e1408fd3fc5a9b98ee4ce
ongiabao2207 <ongiabao36@gmail.com>
2023-04-09 01:03:28 +0700

    Update func ViewCoursesOfAstudent (output bang du lieu)

--- main/File_cpp/Student_view.cpp
+++ main/File_cpp/Student_view.cpp
@@ -3,27 +3,35 @@
 //task 14: view the list of courses of a stundent in a semester
 void ViewCoursesOfAStudent(Account *accHead, Course *courseHead) //courseHead in a semester, accHead is current account after the student login
 {
+    for (int i = 1; i <= 112; i++)
+        cout << "=";
+    cout << "\n";
+    cout << "|    ID" << setw(20) << "Name"<< setw(21) << "Credit" << setw(11) << "Day" << setw(15)  << "Session"  << setw(10) << "Room" << setw(15) << "Teacher" << setw(13) << "|" << endl; 
+    for (int i = 1; i <= 112; i++)
+        cout << "=";
+    cout << "\n";
     while (courseHead)
     {
         while (courseHead->studentCourse)
         {
             if (courseHead->studentCourse->ID == accHead->username)
             {
-                cout << "Course's name: " << courseHead->Name << endl;
-                cout << "ID: " << courseHead->CourseID << endl;
-                cout << "Number of credits: " << courseHead->Credits << endl;
-                cout << "Teacher's name: " << courseHead->TeacherName << endl;
-                cout << "Room: " << courseHead->Room << endl;
-                cout << "Number of students: " << courseHead->numStudents << endl;
-                cout << "Day: " << courseHead->Day << endl;
-                cout << "Session: " << courseHead->Session << endl;
-                cout << "\n\n";
+                cout << "| " << courseHead->CourseID;
+                cout << "       " << courseHead->Name;
+                cout << setw(29 - courseHead->Name.length()) << right << courseHead->Credits;
+                cout << setw(13) << right << courseHead->Day;
+                cout << setw(12) << right << courseHead->Session;   
+                cout << setw(12) << right << courseHead->Room;  
+                cout << "         " << courseHead->TeacherName << setw(20 - courseHead->TeacherName.length()) << "|";       
+                cout << "\n";
                 break;
             }
             courseHead->studentCourse = courseHead->studentCourse->next;
         }
         courseHead = courseHead->next;
     }
+    for (int i = 1; i <= 112; i++)
+        cout << "=";
 }
 
 //task 24: view scoreboard of all course of a student in a semester
@@ -56,57 +64,3 @@ void ViewScoreboard(Account *accHead, Course *courseHead)
     for (int i = 1; i <= 84; i++)
         cout << "=";
 }
-
-// int main()
-// {
-//     Account *accHead = new Account;
-//     accHead->username = "22127026";
-
-//     Course *courseHead = new Course;
-//     courseHead->CourseID = "CSC10001";
-//     courseHead->Name = "Nhap mon lap trinh";
-//     courseHead->studentCourse = new StudentCourse;
-//     courseHead->studentCourse->ID = "22127001";
-//     courseHead->studentCourse->FullName = "Nguyen Van A";
-//     courseHead->studentCourse->next = new StudentCourse;
-//     courseHead->studentCourse->next->ID = "22127026";
-//     courseHead->studentCourse->next->ScoreBoardCourse.midMark = 9.1;
-//     courseHead->studentCourse->next->ScoreBoardCourse.finalMark = 10;
-//     courseHead->studentCourse->next->ScoreBoardCourse.otherMark = 8;
-//     courseHead->studentCourse->next->ScoreBoardCourse.totalMark = 9.9;
-
-//     courseHead->next = new Course;
-//     courseHead->next->CourseID = "BAA0010";
-//     courseHead->next->Name = "Triet hoc Mac Le-nin";
-//     courseHead->next->studentCourse = new StudentCourse;
-//     courseHead->next->studentCourse->ID = "22127001";
-//     courseHead->next->studentCourse->FullName = "Nguyen Van A";
-//     courseHead->next->studentCourse->next = new StudentCourse;
-//     courseHead->next->studentCourse->next->ID = "22127020";
-//     courseHead->next->studentCourse->next->FullName = "Le thi C";
-//     courseHead->next->studentCourse->next->ScoreBoardCourse.midMark = 5;
-//     courseHead->next->studentCourse->next->ScoreBoardCourse.finalMark = 6.5;
-//     courseHead->next->studentCourse->next->ScoreBoardCourse.otherMark = 8;
-//     courseHead->next->studentCourse->next->ScoreBoardCourse.totalMark = 7;
-
-//     courseHead->next->next = new Course;
-//     courseHead->next->next->CourseID = "CSC10002";
-//     courseHead->next->next->Name = "Ky thuat lap trinh";
-//     courseHead->next->next->studentCourse = new StudentCourse;
-//     courseHead->next->next->studentCourse->ID = "22127026";
-//     courseHead->next->next->studentCourse->FullName = "On Gia Bao";
-//     courseHead->next->next->studentCourse->ScoreBoardCourse.midMark = 6;
-//     courseHead->next->next->studentCourse->ScoreBoardCourse.finalMark = 7;
-//     courseHead->next->next->studentCourse->ScoreBoardCourse.otherMark = 8;
-//     courseHead->next->next->studentCourse->ScoreBoardCourse.totalMark = 9;
-//     courseHead->next->next->studentCourse->next = new StudentCourse;
-//     courseHead->next->next->studentCourse->next->ID = "22127275";
-//     courseHead->next->next->studentCourse->next->FullName = "Tran Anh Minh";
-//     courseHead->next->next->studentCourse->next->ScoreBoardCourse.midMark = 10;
-//     courseHead->next->next->studentCourse->next->ScoreBoardCourse.finalMark = 10;
-//     courseHead->next->next->studentCourse->next->ScoreBoardCourse.otherMark = 10;
-//     courseHead->next->next->studentCourse->next->ScoreBoardCourse.totalMark = 10;
-
-//     ViewScoreboard(accHead, courseHead);
-//     return 0;
-// }
